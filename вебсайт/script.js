071f8f9a3bebfd001f4b293ea308d2e9ee99bc86
matzhananar <anr.matzhn@gmail.com>
2021-06-19 18:43:24 +0600

    2nd commit

---
+++ вебсайт/script.js
@@ -0,0 +1,19 @@
+function authorization() {
+    var x = document.getElementById("auth_form");
+
+    if(x.style.display === "none"){
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function basket() {
+    var x = document.getElementById("basket");
+
+    if(x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
